2219571eae4d15bc487e2b8ea7903e7ccb28b2d6
kadar naruto <khadarpaashe123@gmail.com>
2025-03-05 02:45:51 +0300

    defined the prop Types in question,startScreen,Options,nextButton files

--- src/components/tournament/StartScreen.tsx
+++ src/components/tournament/StartScreen.tsx
@@ -1,14 +1,12 @@
-// function StartScreen() {
-//   return <div></div>;
-// }
+import { ActionType,ActionKind } from "@/components/tournament/StartQuestions";
 
-// export default StartScreen;
-
-// import { useQuiz } from "../contexts/QuizContext";
-
-function StartScreen({ numQuestions, dispatch }) {
-  //   const { numQuestions, dispatch } = useQuiz();
-
+function StartScreen({
+  numQuestions,
+  dispatch,
+}: {
+  numQuestions: number;
+  dispatch: React.Dispatch<ActionType>;
+}) {
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-3xl font-bold mb-2">
@@ -17,7 +15,7 @@ function StartScreen({ numQuestions, dispatch }) {
       <h3 className="mb-4">{numQuestions} questions for this major</h3>
       <button
         className="btn btn-ui"
-        onClick={() => dispatch({ type: "start" })}
+        onClick={() => dispatch({ type: ActionKind.start })}
       >
         Let&apos;s start
       </button>
