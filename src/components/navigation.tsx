b7216013f54981a6e600a583bfb1e5c05c1c52b7
kadar naruto <khadarpaashe123@gmail.com>
2025-03-08 03:14:01 +0300

    added userButton component from clerk

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Navigation() {
   return (
     <div className="navbar bg-base-100">
-  <div className="flex-1">
-<Logo/>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><Link href="/majors">Major</Link></li>
-
-    </ul>
-  </div>
-</div>
-  )
+      <div className="flex-1">
+        <Logo />
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal gap-4 items-center px-1">
+          <li className="bg-base-200 ">
+            <Link href="/majors">Major</Link>
+          </li>
+          <UserButton />
+        </ul>
+      </div>
+    </div>
+  );
 }
