539bf909a4c6faddaacb0d78be7b8eb99bfb6abb
kadar naruto <khadarpaashe123@gmail.com>
2025-03-12 12:16:17 +0300

    fixed the logo alligment in the tornament ui

--- src/components/tournament/StartQuestions.tsx
+++ src/components/tournament/StartQuestions.tsx
@@ -155,7 +155,7 @@ export default function StartQuiz({ data }: { data: Question[] }) {
     <div className="bg-[#33479D] h-screen ">
       <div className="flex justify-between items-center">
         <div className="bg-[url('/ramadan-dec.png')] bg-cover bg-center h-60 w-[354px] -translate-x-12"></div>
-        <div className="h-26 w-26 bg-[#FBE726] rounded-full lg:-translate-x-72 md:-translate-x-40">
+        <div className="h-26 w-26 bg-[#FBE726] rounded-full -translate-x-[35%] ">
           <Logo />
         </div>
       </div>
@@ -164,7 +164,6 @@ export default function StartQuiz({ data }: { data: Question[] }) {
         {status === "error" && <Error />}
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
-          // {status}
         )}
         {status === "active" && (
           <>
