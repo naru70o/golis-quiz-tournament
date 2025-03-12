36be533246abb410c8d9db15595b7308396ab354
kadar naruto <khadarpaashe123@gmail.com>
2025-03-12 13:07:09 +0300

    flexed major name with the pencil

--- src/ui/setStatusActive.tsx
+++ src/ui/setStatusActive.tsx
@@ -6,10 +6,10 @@ import React from 'react'
 export default function SetStatusActive({_id}: { _id: string }) {
   return (
     <button
-      className="text-[10px] tracking-widest py-1 px-2 primary-content rounded-full cursor-pointer hover:underline-offset-1"
+      className="flex items-center text-[10px] tracking-widest py-1 px-2 primary-content rounded-full cursor-pointer hover:underline-offset-1"
       onClick={async () => majorSetStatusActive(_id)}
     >
-      Start
+      <span>Start</span>
       <ArrowRight className="inline-block h-2 w-2 hover:opacity-40 transition-all duration-150 ease-in-out cursor-pointer" />
     </button>
   );
