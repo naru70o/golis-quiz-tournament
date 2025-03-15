d42da80bc4a82a91c3804775dc58ef3e2e6718ba
kadar naruto <khadarpaashe123@gmail.com>
2025-03-15 04:37:46 +0300

    added the challenge schema

---
+++ src/lib/schemas/model.challenge.ts
@@ -0,0 +1,17 @@
+import mongoose, {Schema} from 'mongoose';
+
+export const ChallengeSchema= new Schema({
+    challenge:{
+        type: String,
+        required: true,
+   },
+   number:{
+       type: Number,
+       required: true,
+   }
+})
+
+
+const Challenge=mongoose.models.challenge || mongoose.model('Challenge',ChallengeSchema);
+
+export default Challenge;
