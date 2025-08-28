2c6badc6fcc28d686338e049498a9ecd0b7af76d
kadar-naruto <khadarpaashe123@gmail.com>
2025-08-28 19:00:33 +0300

    changed the background

--- src/components/tournament/StartScreen.tsx
+++ src/components/tournament/StartScreen.tsx
@@ -1,6 +1,5 @@
 import { starQuiz } from "@/state/quizSlice";
 import { AppDispatch } from "@/state/store";
-import { Dispatch } from "@reduxjs/toolkit";
 import { ArrowRight } from "lucide-react";
 
 function StartScreen({
@@ -15,7 +14,10 @@ function StartScreen({
       <h2 className='text-9xl font-bold mb-2'>
         <span className='text-[200px] font-semibold'>Q</span>uizzler{" "}
       </h2>
-      <h3 className='flex gap-1 items-center text-3xl mb-4 font-semibold select-none cursor-pointer hover:text-gray-600 transition-all'>
+      <h3
+        className='flex gap-1 items-center text-3xl mb-4 font-semibold select-none cursor-pointer hover:text-gray-600 transition-all'
+        onClick={() => dispatch(starQuiz())}
+      >
         Let&apos;s start the quiz{" "}
         <span>
           <ArrowRight />
