0f5a415e24cf33f93f8a73d98cdf7b9c9a2a17b7
naruto <khadarpaashe123@gmail.com>
2025-11-16 18:22:16 +0300

    fixed: renamed the question index to stay away from shadowing the component (index) prop

--- src/components/tournament/Options.tsx
+++ src/components/tournament/Options.tsx
@@ -16,21 +16,25 @@ const optionIdentifier = (index: number) => {
 };
 
 function Options() {
-  const { questions, dispatch, answer, index } = useStoreState();
+  const { questions, dispatch, answer, index: questionIndex } = useStoreState();
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   const hasAnswered = answer !== null;
-  console.log("answer ...", questions[index].options);
+  console.log(
+    "answer ...",
+    `here is the index ${questionIndex}`,
+    questions[questionIndex]?.options
+  );
   return (
     <div className="grid grid-cols-2 gap-y-9 gap-x-32 mb-[3.2rem] container max-w-7xl mx-auto -translate-y-[20%]">
-      {questions[index].options &&
-        questions[index].options.map((option, index) => {
+      {questions[questionIndex]?.options &&
+        questions[questionIndex].options.map((option, optionIndex) => {
           return (
             <button
               className={`bg-[#FBE726] text-black min-w-[300px] min-h-[108px] rounded-3xl relative  ${
                 hasAnswered
-                  ? index === questions[index].correctOptionIndex
+                  ? optionIndex === questions[questionIndex].correctOptionIndex
                     ? "bg-green-500 "
-                    : selectedIndex === index
+                    : selectedIndex === optionIndex
                     ? "bg-red-500 animate-pulse"
                     : ""
                   : ""
@@ -39,12 +43,12 @@ function Options() {
               key={option._id}
               disabled={hasAnswered}
               onClick={() => {
-                setSelectedIndex(index);
-                dispatch(newAnswer(index));
+                setSelectedIndex(optionIndex);
+                dispatch(newAnswer(optionIndex));
               }}
             >
-              <div className="absolute flex items-center justify-center top-0 lef-0 text-white text-center text-5xl font-bold bg-[#33479D] h-full w-[20%] rounded-3xl border-4 border-[#FBE726]">
-                <div className="">{optionIdentifier(index)}</div>
+              <div className="absolute flex items-center justify-center top-0 left-0 text-white text-center text-5xl font-bold bg-[#33479D] h-full w-[20%] rounded-3xl border-4 border-[#FBE726]">
+                <div className="">{optionIdentifier(optionIndex)}</div>
               </div>
               <div className="text-3xl text-center font-bold ml-[25%]">
                 {option.text}
